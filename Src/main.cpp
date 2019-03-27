06619ee17180cc5546df7dab232d14076f4ec8fd
Trobin <robin.tissot@free.fr>
2019-03-27 16:45:42 +0100

    Ajout du mode vu au dessus et vu 3D

--- Src/main.cpp
+++ Src/main.cpp
@@ -22,9 +22,13 @@ static int wPy = 50; // Position verticale de la fenetre
 static float px = 0;
 static float py = 10;
 static float pz = 0;
-static const float blanc[] = {1.0F, 1.0F, 1.0F, 1.0F};
+static float ox = 0;
+static float oy = 0;
+static float oz = 0;
+static const float blanc[] = { 1.0F, 1.0F, 1.0F, 1.0F };
 static const int nbMorceau = 9;
-
+static bool vueDessus = true;
+static int oldMX = -1, oldMY = -1;
 static MorceauParcours *parcours[nbMorceau];
 static int definit = 0;
 /* Fonction d'initialisation des parametres     */
@@ -33,13 +37,13 @@ static int definit = 0;
 /* du programme                                 */
 
 static void init(void) {
-    glClearColor(0.25F, 0.25F, 0.25F, 1.0F);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, blanc);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glDepthFunc(GL_LESS);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_NORMALIZE);
+	glClearColor(0.25F, 0.25F, 0.25F, 1.0F);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, blanc);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_NORMALIZE);
 }
 
 /* Scene dessinee                               */
@@ -60,19 +64,24 @@ static void scene(void) {
 /* de la fenetre de dessin                      */
 
 static void display(void) {
-    printf("D\n");
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    const GLfloat light0_position[] = {0.0, 0.0, 10.0, 1.0};
-    glPushMatrix();
-    glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
-    gluLookAt(px, py, pz, px, 0, pz, 0.0, 0.0, -1.0);
-    scene();
-    glPopMatrix();
-    glFlush();
-    glutSwapBuffers();
-    int error = glGetError();
-    if (error != GL_NO_ERROR)
-        printf("Attention erreur %d\n", error);
+	printf("D\n");
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	const GLfloat light0_position[] = { 0.0, 0.0, 10.0, 1.0 };
+	glPushMatrix();
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	if (vueDessus) {
+		gluLookAt(px, py, pz, px, 0, pz, 0.0, 0.0, -1.0);
+	}
+	else {
+		gluLookAt(px, py, pz, ox, oy, oz, 0.0, 0.0, -1.0);
+	}
+	scene();
+	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
+	int error = glGetError();
+	if (error != GL_NO_ERROR)
+		printf("Attention erreur %d\n", error);
 }
 
 /* Fonction executee lors d'un changement       */
@@ -80,17 +89,17 @@ static void display(void) {
 /* de la taille de la fenetre OpenGL            */
 
 static void reshape(int tx, int ty) {
-    printf("R\n");
-    glViewport(0, 0, tx, ty);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    double ratio = (double) tx / ty;
-    if (ratio >= 1.0)
-        gluPerspective(80.0, ratio, 1.0, 50.0);
-    else
-        gluPerspective(80.0 / ratio, ratio, 1.0, 50.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	printf("R\n");
+	glViewport(0, 0, tx, ty);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double ratio = (double)tx / ty;
+	if (ratio >= 1.0)
+		gluPerspective(80.0, ratio, 1.0, 50.0);
+	else
+		gluPerspective(80.0 / ratio, ratio, 1.0, 50.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 /* Fonction executee lorsqu'aucun evenement     */
@@ -98,8 +107,8 @@ static void reshape(int tx, int ty) {
 /* n'est en file d'attente                      */
 
 static void idle(void) {
-    printf("I\n");
-    glutPostRedisplay();
+	printf("I\n");
+	glutPostRedisplay();
 }
 
 /* Fonction executee lors de l'appui            */
@@ -107,12 +116,14 @@ static void idle(void) {
 /* d'une touche alphanumerique du clavier       */
 
 static void keyboard(unsigned char key, int x, int y) {
-    printf("K  %4c %4d %4d\n", key, x, y);
-    switch (key) {
-        case 0x1B:
-            exit(0);
-            break;
-    }
+	printf("K  %4c %4d %4d\n", key, x, y);
+	switch (key) {
+	case 0x1B:
+		exit(0);
+		break;
+	case 0x20:
+		vueDessus = !vueDessus;
+	}
 }
 
 /* Fonction executee lors de l'appui            */
@@ -122,38 +133,52 @@ static void keyboard(unsigned char key, int x, int y) {
 /*   - touches de fonction                      */
 
 static void special(int key, int x, int y) {
-    switch (key) {
-        case GLUT_KEY_UP:
-            pz -= 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_DOWN:
-            pz += 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_LEFT:
-            px -= 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_RIGHT:
-            px += 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_PAGE_UP:
-            py -= 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_PAGE_DOWN:
-            py += 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_F1:
-            px = 0.0;
-            py = 10.0;
-            pz = 0.0;
-            glutPostRedisplay();
-            break;
-    }
+	switch (key) {
+	case GLUT_KEY_UP:
+		pz -= 1;
+		if (vueDessus) {
+			oz = pz;
+		}
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_DOWN:
+		pz += 1;
+		if (vueDessus) {
+			oz = pz;
+		}
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_LEFT:
+		px -= 1;
+		if (vueDessus) {
+			ox = px;
+		}
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_RIGHT:
+		px += 1;
+		if (vueDessus) {
+			ox = px;
+		}
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_PAGE_UP:
+		py -= 1;
+		//oy = py;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		py += 1;
+		//oy = py;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_F1:
+		px = 0.0;
+		py = 10.0;
+		pz = 0.0;
+		glutPostRedisplay();
+		break;
+	}
 }
 
 /* Fonction executee lors de l'utilisation      */
@@ -161,7 +186,7 @@ static void special(int key, int x, int y) {
 /* de la souris sur la fenetre                  */
 
 static void mouse(int buton, int state, int x, int y) {
-    printf("M  %4d %4d %4d %4d\n", buton, state, x, y);
+	printf("M  %4d %4d %4d %4d\n", buton, state, x, y);
 }
 
 /* Fonction executee lors du passage            */
@@ -170,7 +195,20 @@ static void mouse(int buton, int state, int x, int y) {
 /* avec un boutton presse                       */
 
 static void mouseMotion(int x, int y) {
-    printf("MM %4d %4d\n", x, y);
+	printf("MM %4d %4d\n", x, y);
+	if (oldMX < 0) {
+		oldMX = x;
+	}
+	if (oldMY < 0) {
+		oldMY = y;
+	}
+	int diffX = x - oldMX;
+	int diffY = y - oldMY;
+	px += diffX % 2;
+	pz += diffY % 2;
+	oldMX = x;
+	oldMY = y;
+
 }
 
 /* Fonction executee lors du passage            */
@@ -179,7 +217,7 @@ static void mouseMotion(int x, int y) {
 /* sans boutton presse                          */
 
 static void passiveMouseMotion(int x, int y) {
-    printf("PM %4d %4d\n", x, y);
+	printf("PM %4d %4d\n", x, y);
 }
 
 /* Fonction executee automatiquement            */
@@ -187,13 +225,13 @@ static void passiveMouseMotion(int x, int y) {
 /* lors de l'execution de la fonction exit()    */
 
 static void clean(void) {
- //   delete ligneTest;
-//    delete virage;
-    printf("Bye\n");
+	//   delete ligneTest;
+   //    delete virage;
+	printf("Bye\n");
 }
 
 static void createParcours() {
-	parcours[0] = new MorceauParcoursLigne(Pos3D(0.0, 0.0, 0.0) , 8, 42, MorceauParcours::Direction::EST);
+	parcours[0] = new MorceauParcoursLigne(Pos3D(0.0, 0.0, 0.0), 8, 42, MorceauParcours::Direction::EST);
 	parcours[1] = new MorceauParcoursVirage(Pos3D(42.0, 0.0, -8.0), 8, 16, MorceauParcours::Direction::EST, MorceauParcours::Direction::NORD);
 	parcours[2] = new MorceauParcoursLigne(Pos3D(50.0, 0.0, -36.0), 8, 24, MorceauParcours::Direction::NORD);
 
@@ -202,22 +240,22 @@ static void createParcours() {
 /* Fonction principale                          */
 
 int main(int argc, char **argv) {
-    atexit(clean);
-    glutInit(&argc, argv);
-	createParcours();    
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(wTx, wTy);
-    glutInitWindowPosition(wPx, wPy);
-    glutCreateWindow("Projet Kart");
-    init();
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(special);
-    glutMouseFunc(mouse);
-    glutMotionFunc(mouseMotion);
-    //glutPassiveMotionFunc(passiveMouseMotion);
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutIdleFunc(idle);
-    glutMainLoop();
-    return (0);
+	atexit(clean);
+	glutInit(&argc, argv);
+	createParcours();
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(wTx, wTy);
+	glutInitWindowPosition(wPx, wPy);
+	glutCreateWindow("Projet Kart");
+	init();
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(special);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	//glutPassiveMotionFunc(passiveMouseMotion);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+	glutMainLoop();
+	return (0);
 }
