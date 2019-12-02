f7b71c5b162be0fd6f4ebef0a6f2bc3d6e62158b
Leslie-Anne <leslie-anne39@hotmail.fr>
2019-12-02 22:27:34 +0100

    Modification du CSVReader pour la lecture de conteneurs dans un fichier

--- Include/3D/CSVReader3D.h
+++ Include/3D/CSVReader3D.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <3D/Composant3D.h>
+#include <3D/Conteneur3D.h>
 using namespace std;
 #ifndef ____CSVREADER3D____
 #define ____CSVREADER3D____
@@ -14,6 +15,7 @@ class CSVReader3D {
 	protected:
 		std::string filename;
 		std::vector <Composant3D *> listComposant;
+		std::vector <Conteneur3D *> listConteneur;
 
 	public:
 
@@ -29,15 +31,19 @@ class CSVReader3D {
 
 		virtual bool setFilename(std::string filename);
 		virtual bool setListComposant(std::vector <Composant3D *> listComposant);
+		virtual bool setListConteneur(std::vector <Conteneur3D *> listConteneur);
 
 		/* Getters                                  */
 
 		std::string getFilename();
 		std::vector <Composant3D *> getListComposant();
+		std::vector <Conteneur3D *> getListConteneur();
 
-		void lireCSV();
+		void lireCSV(int type);
 		void ajoutComposant(std::vector <string> row);
-		void verifCompoVector(std::vector <Composant3D *> liste);
+		void ajoutConteneur(std::vector <string> row);
+		void verifCompoVectorComposant(std::vector <Composant3D *> liste);
+		void verifCompoVectorConteneur(std::vector <Conteneur3D *> liste);
 };
 
 #endif
