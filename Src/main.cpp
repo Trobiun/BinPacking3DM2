5b9f272ae5637a2c788599be2f2f11b57fa2c57b
keitarn <florian.joriot@hotmail.fr>
2020-01-20 13:15:27 +0100

    retirer variable inutile

--- Src/main.cpp
+++ Src/main.cpp
@@ -30,22 +30,11 @@ static float pz = 30;
 static float ox = 25;
 static float oy = 25;
 static float oz = 0;
-static float ax = 0;
-static float ay = 0;
-static float az = 0;
 static bool keys[6] = {false};
 static bool keyboardKeys[256] = {false};
-static int oldMX = -1, oldMY = -1;
-static int deplMX = 0, deplMY = 0;
-static bool cameraMove = false;
 static bool opaque = false;
-static float zoom[3] = {3.0, 3.0, 3.0};
-static double normeCamera = 1.0;
 static const GLfloat blanc[] = {1.0F, 1.0F, 1.0F, 1.0F};
 static const GLfloat vert[4] = {0.0F, 1.0F, 0.0F, 1.0F};
-static const GLfloat jaune[4] = {1.0F, 1.0F, 0.0F, 1.0F};
-static const GLfloat magenta[4] = {0.0F, 1.0F, 0.0F, 1.0F};
-static const GLfloat cyan[4] = {0.0F, 1.0F, 0.0F, 1.0F};
 static const GLfloat light0_position[4] = {0.0, 0.0, 10.0, 1.0};
 
 static float r = 50;
@@ -65,8 +54,6 @@ static std::list<Conteneur*> conteneursDispo;
 static std::list<Conteneur3D*> conteneurs3D;
 static std::list<Conteneur3D*> conteneurs3DDispo;
 static std::list<Composant*> composants;
-static std::list<Composant*> restants;
-
 static std::list<Composant3D*> composants3D;
 
 #define KEY_LEFT 0
@@ -96,7 +83,6 @@ static void init(void) {
 static void courantConteneur() {
     if (affichage3Dou2D == 0) {
         Position2D* posCont = (*posCont2D)->getPosition();
-
         px = (*posCont2D)->getCoteX() / 2 + posCont->getX();
         py = (*posCont2D)->getCoteY() / 2 + posCont->getY();
         pz = 30;
@@ -181,14 +167,9 @@ static void display(void) {
     glPushMatrix();
     glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
 
-    //TODO VECTEUR entre (px, py, pz) et (ox, oy, oz) puis calcule les valeur a ajoute a px, py et pz pour parcourir ce vecteur sur une distance de zoom
-
-    gluLookAt(px /*+ zoom[0]*/, py /*+ zoom[1]*/, pz /*+ zoom[2]*/, ox /*+ zoom[0]*/, oy/* + zoom[1]*/, oz/* + zoom[2]*/, 0.0, 1.0, 0.0);
+    gluLookAt(px, py, pz, ox, oy, oz, 0.0, 1.0, 0.0);
 
     scene();
-    ax = 0;
-    ay = 0;
-    az = 0;
     glPopMatrix();
     glFlush();
     glutSwapBuffers();
@@ -223,55 +204,29 @@ static void reshape(int wx, int wy) {
 
 static void idle(void) {
     if (keys[KEY_UP]) {
-        //        py -= 0.25;
         anglex--;
     }
     if (keys[KEY_DOWN]) {
-        //        py += 0.25;
         anglex++;
     }
     if (keys[KEY_LEFT]) {
-        //        px -= 0.25;
         angley++;
     }
     if (keys[KEY_RIGHT]) {
-        //        px += 0.25;
         angley--;
     }
     if (keys[KEY_PAGE_UP]) {
         float xPres = px - ox;
         float yPres = py - oy;
         float zPres = pz - oz;
-        zoom[0] -= xPres * 0.5;
-        zoom[1] -= yPres * 0.5;
-        zoom[2] -= zPres * 0.5;
         r--;
     }
     if (keys[KEY_PAGE_DOWN]) {
         float xPres = px - ox;
         float yPres = py - oy;
         float zPres = pz - oz;
-        zoom[0] += xPres * 0.5;
-        zoom[1] += yPres * 0.5;
-        zoom[2] += zPres * 0.5;
         r++;
     }
-    //    if (keyboardKeys['z']) {
-    //        py += 0.5;
-    //        oy = py;
-    //    }
-    //    if (keyboardKeys['s']) {
-    //        py -= 0.5;
-    //        oy = py;
-    //    }
-    //    if (keyboardKeys['q']) {
-    //        px -= 0.5;
-    //        ox = px;
-    //    }
-    //    if (keyboardKeys['d']) {
-    //        px += 0.5;
-    //        ox = px;
-    //    }
     glutPostRedisplay();
 }
 
@@ -398,32 +353,6 @@ static void special(int specialKey, int x, int y) {
 
 static void mouse(int button, int state, int x, int y) {
    // printf("M  %4d %4d %4d %4d\n", button, state, x, y);
-    if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
-        cameraMove = true;
-        px = ox;
-        pz = oz;
-    }
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        cameraMove = false;
-        ox = px;
-        oz = pz;
-    }
-    if (button == 3 && state == GLUT_DOWN) {
-        float xPres = px - ox;
-        float yPres = py - oy;
-        float zPres = pz - oz;
-        zoom[0] -= xPres * 0.1;
-        zoom[1] -= yPres * 0.1;
-        zoom[2] -= zPres * 0.1;
-    }
-    if (button == 4 && state == GLUT_DOWN) {
-        float xPres = px - ox;
-        float yPres = py - oy;
-        float zPres = pz - oz;
-        zoom[0] += xPres * 0.1;
-        zoom[1] += yPres * 0.1;
-        zoom[2] += zPres * 0.1;
-    }
 }
 
 /* Fonction executee lors du passage            */
@@ -433,24 +362,6 @@ static void mouse(int button, int state, int x, int y) {
 
 static void mouseMotion(int x, int y) {
     //printf("MM %4d %4d\n", x, y);
-    if (oldMX < 0) {
-        oldMX = x;
-    }
-    if (oldMY < 0) {
-        oldMY = y;
-    }
-    if (x < oldMX) {
-        deplMX = -1;
-    }
-    if (x > oldMX) {
-        deplMX = 1;
-    }
-    int diffX = x - oldMX;
-    int diffY = y - oldMY;
-    px += diffX % 3;
-    pz += diffY % 3;
-    oldMX = x;
-    oldMY = y;
 }
 
 /* Fonction executee lors du passage            */
@@ -479,22 +390,6 @@ static void clean(void) {
 	else {
         delete algo3D;
     }
-    /*std::list<Conteneur*>::iterator it;
-    for (it = conteneurs.begin(); it != conteneurs.end(); it++) {
-        //delete *it;
-    }
-    //conteneurs.clear();
-    std::list<Composant*>::iterator it2;
-    for (it2 = composants.begin(); it2 != composants.end(); it2++) {
-        //delete *it2;
-    }
-    //composants.clear();*/
-
-    /*std::list<Composant*>::iterator it3;
-    for (it3 = restants.begin(); it3 != restants.end(); it3++) {
-        //delete *it3;
-    }*/
-    //restants.clear();
     if (algo != NULL) {
     }
     if (algo3D != NULL) {
@@ -649,22 +544,21 @@ static void lectureCSVComposant(std::string filename) {
         nofin = addCont(idCont);
         idCont++;
         algo->setListeConteneur(conteneurs);
-        std::list<Composant*> reste = algo->calculRangement();
-        algo->setListeComposant(reste);
+        std::list<Composant*> composants = algo->calculRangement();
+        algo->setListeComposant(composants);
 
-        while (!(reste.empty()) && nofin) {
+        while (!(composants.empty()) && nofin) {
 
             nofin = addCont(idCont);
             idCont++;
             algo->setListeConteneur(conteneurs);
-            reste = algo->calculRangement();
-            algo->setListeComposant(reste);
+			composants = algo->calculRangement();
+            algo->setListeComposant(composants);
         }
         posCont2D = conteneurs.begin();
 
-        restants.insert(restants.begin(), reste.begin(), reste.end());
         verifCompoVector(listeDesComposant);
-        verifCompoList(restants, -1);
+        verifCompoList(composants, -1);
         verifCompoList((*posCont2D)->getListComposant(), 1);
         posCont2D = conteneurs.begin();
 
